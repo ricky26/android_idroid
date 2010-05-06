b21a358251f631c6909d1abf9401b02d5d0b404e
Patrick Wildt <webmaster@patrick-wildt.de>
2010-05-06 08:08:32 -0700

    Removed unneeded things

--- iphone/Android.mk
+++ iphone/Android.mk
@@ -3,22 +3,4 @@
 include $(call all-subdir-makefiles)
 LOCAL_PATH := $(call my-dir)
 #
-# this is here to use the pre-built kernel
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-#TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
-TARGET_PREBUILT_KERNEL := /home/patrick/work/kernel_common/arch/arm/boot/zImage
-endif
-#
-file := $(INSTALLED_KERNEL_TARGET)
-ALL_PREBUILT += $(file)
-$(file): $(TARGET_PREBUILT_KERNEL) | $(ACP)
-      	$(transform-prebuilt-to-target)
-#
-# no boot loader, so we don't need any of that stuff..  
-#
-LOCAL_PATH := vendor/apple/iphone
-#
 include $(CLEAR_VARS)
-#
-# include more board specific stuff here? Such as Audio parameters.      
-#
