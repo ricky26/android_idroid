b9b35181eb1beee5cea95236150ca58e49e08e2b
ricky26 <rickytaylor26@gmail.com>
2010-05-07 21:33:36 +0100

    Added keymap compilation to AndroidBoard.mk.

--- iPhone3G/AndroidBoard.mk
+++ iPhone3G/AndroidBoard.mk
@@ -1,3 +1,13 @@
 include vendor/idroid/common/AndroidBoardCommon.mk
 
 include vendor/idroid/iPhone3G/libaudio/Android.mk
+
+file := $(TARGET_OUT_KEYLAYOUT)/gpio-keys.kl
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/gpio-keys.kl | $(ACP)
+        $(transform-prebuilt-to-target)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := gpio-keys.kcm
+include $(BUILD_KEY_CHAR_MAP)
+
